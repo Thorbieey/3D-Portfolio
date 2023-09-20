282184b0138b379d0e0441b0670f475174d9a378
Tobi Olajide <tobiolajide887@yahoo.com>
2023-09-20 21:15:33 +0100

    added wrapper and nav id

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import {Tilt} from 'react-tilt';
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
-// import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
@@ -63,5 +63,4 @@ const About = () => {
   );
 };
 
-export default About
-// export default SectionWrapper(About, "about");
+export default SectionWrapper(About, "about");
